ebdcc001d282f7d4d449deebdff9971e78812e30
snan3385 <snan3385@sysco.com>
2022-06-17 17:40:15 +0530

    integrate team app with root app

--- src/dim-connect-root-config.js
+++ src/dim-connect-root-config.js
@@ -1,16 +1,16 @@
 import { registerApplication, start } from "single-spa";
 
-// registerApplication({
-//   name: "@dim-connect/admin-portal",
-//   app: () => System.import("@dim-connect/admin-portal"),
-//   activeWhen: ["/"],
-// });
-//
-// registerApplication({
-//   name: "@dim-connect/apic-user-onboarding",
-//   app: () => System.import("@dim-connect/apic-user-onboarding"),
-//   activeWhen: ["/apic-user-onboarding"],
-// });
+registerApplication({
+  name: "@dim-connect/admin-portal",
+  app: () => System.import("@dim-connect/admin-portal"),
+  activeWhen: ["/"],
+});
+
+registerApplication({
+  name: "@dim-connect/apic-user-onboarding",
+  app: () => System.import("@dim-connect/apic-user-onboarding"),
+  activeWhen: ["/apic-user-onboarding"],
+});
 
 registerApplication({
   name: "@team-app/team-app",
